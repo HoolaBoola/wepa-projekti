6ab863281d12032000f51a94ab5cb95245d922b2
Jaime <jaime.heikkiladias@helsinki.fi>
2019-11-17 23:53:02 +0200

    Login and register barebones

--- src/main/java/projekti/DefaultController.java
+++ src/main/java/projekti/DefaultController.java
@@ -3,6 +3,8 @@ package projekti;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DefaultController {
@@ -21,6 +23,16 @@ public class DefaultController {
     @GetMapping("/register")
     public String register(Model model) {
         return "registering";
+    }
+    
+    @PostMapping("/register")
+    public String postRegister(@RequestParam String name, @RequestParam String password, @RequestParam String merkkijono){
+        return "redirect:/";
+    }
+    
+    @PostMapping("/login")
+    public String postLogin(@RequestParam String name, @RequestParam String password){
+        return "redirect:/";
     }
 
 }
