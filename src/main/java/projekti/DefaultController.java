66292b4801fae4c769d5e17930d0c51b4f9355f2
Jaime <jaime.heikkiladias@helsinki.fi>
2019-11-17 23:33:46 +0200

    Initial commit

---
+++ src/main/java/projekti/DefaultController.java
@@ -0,0 +1,26 @@
+package projekti;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DefaultController {
+
+    @GetMapping("/")
+    public String helloWorld(Model model) {
+        model.addAttribute("message", "World!");
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        return "registering";
+    }
+
+}
