9c3edf388a18ae70f9e7a871ab46157eba5536ec
Jaime <jaime.heikkiladias@helsinki.fi>
2019-11-26 23:24:28 +0200

    Now it should all be properly deleted

--- src/test/java/projekti/AccountTest.java
+++ src/test/java/projekti/AccountTest.java
@@ -7,8 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
-import projekti.entities.Account;
-import projekti.services.AccountService;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -17,20 +16,5 @@ public class AccountTest extends org.fluentlenium.adapter.junit.FluentTest {
     @LocalServerPort
     private Integer port;
     
-    @Autowired
-    private AccountService accService;
-    
-    @Test
-    public void luoAccount(){
-        Account account = new Account();
-        
-        account.setName("James Bond");
-        account.setMerkkijono("Ja");
-        
-        accService.list().forEach(acc -> System.out.println(acc));
-        
-        accService.addAccount(account);
-        
-        assertTrue(accService.list().contains(account));
-    }
+
 }
