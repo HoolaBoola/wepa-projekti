793320f7abf7bff1f99540c21cc7c06d192037a5
Jaime <jaime.heikkiladias@helsinki.fi>
2019-11-26 23:08:02 +0200

    Deleted imports

--- src/main/java/projekti/DefaultController.java
+++ src/main/java/projekti/DefaultController.java
@@ -1,16 +1,8 @@
 package projekti;
 
-import java.util.ArrayList;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import projekti.entities.Account;
-import projekti.services.AccountService;
 
 @Controller
 public class DefaultController {
