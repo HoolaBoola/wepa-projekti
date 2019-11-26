5c52ee5204aeec0db1c8a9e5c6d23450389de715
Jaime <jaime.heikkiladias@helsinki.fi>
2019-11-26 18:56:14 +0200

    OIjoijoöi

--- src/main/java/projekti/SecurityConfiguration.java
+++ src/main/java/projekti/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -21,6 +22,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
  * @author hajajaim
  */
 @Profile("default")
+@Order(2)
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
